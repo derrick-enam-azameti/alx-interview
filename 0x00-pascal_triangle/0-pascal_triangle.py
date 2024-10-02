ed88876baabd3727212c98bc133c45bb5f23a9fa
Derrick Enam Azameti <azametid@gmail.com>
2024-10-02 21:34:12 +0000

    Add: 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Returns a list of lists of integers representing
+the Pascal’s triangle of `n`:
+
+Returns an empty list if n <= 0
+"""
+
+def pascal_triangle(n):
+    pass
