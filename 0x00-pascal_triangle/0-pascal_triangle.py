2ddf542006148bafb097d765db3ca37115370f60
Derrick Enam Azameti <azametid@gmail.com>
2024-10-03 19:38:18 +0000

    Passing the checkers

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,31 +9,14 @@ Returns:
 
 
 def pascal_triangle(n):
-    """
-    Generates Pascal's triangle up to the nth row.
-
-    Args:
-        n (int): The number of rows to generate.
-
-    Returns:
-        list of lists: Shows integers representing Pascal's triangle.
-    """
-    if n <= 0:
-        return []
-
-    res = [[1]]  # Initialize the first row
-
-    # Generate the rows
-    for i in range(1, n):
-        # Start the row with 1
-        row = [1]
-        # Calculate the intermediate values for each row using the previous row
-        for j in range(1, i):
-            row.append(res[i-1][j-1] + res[i-1][j])
-
-        # End the row with 1
-        row.append(1)
-
-        # Append the row to the result
-        res.append(row)
-    return res
+    """Generate Pascal's triangle up to the specified number of rows, n."""
+    triangle = []
+    if n > 0:
+        for current_row in range(1, n + 1):
+            row_values = []
+            coefficient = 1
+            for position in range(1, current_row + 1):
+                row_values.append(coefficient)
+                coefficient = coefficient * (current_row - position) // position
+            triangle.append(row_values)
+    return triangle
