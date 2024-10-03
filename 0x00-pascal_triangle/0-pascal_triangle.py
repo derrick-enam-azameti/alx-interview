cfa3d78d28ec27405a03c38d8cb11d86c7f81a2a
Derrick Enam Azameti <azametid@gmail.com>
2024-10-03 19:34:32 +0000

    Modify: 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,10 +11,10 @@ Returns:
 def pascal_triangle(n):
     """
     Generates Pascal's triangle up to the nth row.
-    
+
     Args:
         n (int): The number of rows to generate.
-    
+
     Returns:
         list of lists: Shows integers representing Pascal's triangle.
     """
@@ -23,15 +23,14 @@ def pascal_triangle(n):
 
     res = [[1]]  # Initialize the first row
 
-    # Generate rows for Pascal's triangle
+    # Generate the rows
     for i in range(1, n):
         # Start the row with 1
-        row = [1]  
-        
+        row = [1]
         # Calculate the intermediate values for each row using the previous row
         for j in range(1, i):
             row.append(res[i-1][j-1] + res[i-1][j])
-        
+
         # End the row with 1
         row.append(1)
 
